5918b0aaf0565023947586587db74164e293b395
pedro <pedrodarioc@gmail.com>
2022-08-13 15:11:40 -0300

    primeros 6

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,13 +1,13 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = vámos;
+const nuevaString = "vamos";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 1405;
 
-// Crea una variable booleana:
-const nuevoBool = masculino;
+// Crea una variable booleana:add
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
